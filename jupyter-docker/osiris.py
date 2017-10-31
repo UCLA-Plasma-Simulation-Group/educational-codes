036896dfa59f0061ee7643988fd36431810844bf
romanplee <31547765+romanplee@users.noreply.github.com>
2017-10-31 08:56:08 -0700

    Add files via upload

--- jupyter-docker/osiris.py
+++ jupyter-docker/osiris.py
@@ -5,9 +5,12 @@ import IPython.display
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-#from os import listdir
+# from os import listdir
 from ipywidgets import interact
 # %matplotlib inline
+# ROMAN'S LIBRARIES
+from h5_utilities import *
+from analysis import *
 
 def runosiris(rundir='',inputfile='osiris-input.txt'):
 
@@ -19,10 +22,19 @@ def runosiris(rundir='',inputfile='osiris-input.txt'):
         return_code = popen.wait()
         if return_code:
             raise subprocess.CalledProcessError(return_code, cmd)
+    
+    def combine_h5(ex):
+        in_file = workdir + '/MS/FLD/' + ex + '/'
+        out_file = workdir + '/' + ex + '.h5'
+        for path in execute(["python", "combine_h5_util_1d.py", in_file, out_file]):
+            IPython.display.clear_output(wait=True)
+            print(path, end='')    
 
     workdir = os.getcwd()
     workdir += '/' + rundir
     print(workdir)
+    
+    # run osiris-1D.e executable
     if(not os.path.isdir(workdir)):
        os.mkdir(workdir)
     if(rundir != ''):
@@ -31,6 +43,12 @@ def runosiris(rundir='',inputfile='osiris-input.txt'):
     for path in execute(["./osiris-1D.e","-w",workdir,"osiris-input.txt"]):
         IPython.display.clear_output(wait=True)
         print(path, end='')
+    
+    # run combine_h5_util_1d.py script for e1/, e2/, e3/
+    combine_h5('e1')
+    combine_h5('e2')
+    combine_h5('e3')
+
     return
 
 
@@ -354,3 +372,147 @@ def wkplot(rundir='',dataset='e3',klim=[-1,-1],wlim=[-1,-1],zlim=[-1,-1],
 #     #oldfig
 #     IPython.display.display(oldfig)
 #     return oldfig
+
+
+# ROMAN'S FUNCTIONS
+def x(n):
+    # returns position, x, given density, n
+    one_0 = 10
+    one_D = 790
+    n_peak = 2
+    x = one_0 + n/n_peak * one_D
+    return x
+
+def gen_path(rundir, plot_or):
+    PATH = os.getcwd() + '/' + rundir
+    if (plot_or==1):
+        PATH += '/e1.h5'
+    elif (plot_or==2):
+        PATH += '/e2.h5'
+    else:
+        PATH += '/e3.h5'
+    return PATH
+
+def plot_xt(rundir, TITLE, b0_mag, plot_or, show_theory):
+    # initialize values
+    PATH = gen_path(rundir, plot_or)
+    hdf5_data = read_hdf(PATH)
+
+    w_0 = 1.0
+    n_L = w_0**2 + w_0*b0_mag
+    n_R = w_0**2 - w_0*b0_mag
+
+    y_vals = np.arange(hdf5_data.axes[1].axis_min, hdf5_data.axes[1].axis_max, 1)
+    x_vals = np.full(len(y_vals), x(n_L))
+    x_vals2 = np.full(len(y_vals), x(n_R))
+    x_vals3 = np.full(len(y_vals), x(1.0))
+    x_vals4 = np.full(len(y_vals), x(w_0**2 - b0_mag**2))
+
+    # create figure
+    plt.figure()
+    plotme(hdf5_data)
+    plt.title(TITLE + ' x-t space' + ' e' + str(plot_or))
+    plt.xlabel('x')
+    plt.ylabel('t')
+    if (show_theory==True):
+        plt.plot(x_vals, y_vals, 'c--', label='$\omega$$_L$ x-cutoff') #L-cutoff
+        plt.plot(x_vals2, y_vals, 'b--', label='$\omega$$_R$ x-cutoff')#R-cutoff
+        plt.plot(x_vals3, y_vals, 'r--', label='$\omega$$_P$ o-cutoff')
+        plt.plot(x_vals4, y_vals, 'g--', label='$\omega$$_h$ o-resonance')
+        plt.legend(loc=0)
+    plt.show()
+
+def plot_log_xt(PATH, TITLE):
+    # initialize values
+    hdf5_data = read_hdf(PATH)
+
+    # log the hdf5 data
+    s = 1e-3  #sensitivity
+    for i in range(hdf5_data.data.shape[0]):
+        for j in range(hdf5_data.data.shape[1]):
+            if abs(hdf5_data.data[i,j]) > s:
+                hdf5_data.data[i,j] = np.log(abs(hdf5_data.data[i,j]))
+            else:
+                hdf5_data.data[i,j] = 0
+
+    # create figure
+    plt.figure()
+    plotme(hdf5_data)
+    plt.title(TITLE + ' x-t space log plot')
+    plt.xlabel('x')
+    plt.ylabel('t')
+    # plt.legend(loc=0)
+    plt.show()
+
+def plot_wk(rundir, TITLE, b0_mag, plot_or, show_theory):
+    # initialize values
+    PATH = gen_path(rundir, plot_or)
+    hdf5_data = read_hdf(PATH)
+    hdf5_data = FFT_hdf5(hdf5_data)   # FFT the data (x-t -> w-k)
+
+    w_p = 1.0                         # plamsa frequency
+    w_c = b0_mag                      # cyclotron freq
+    w_0 = 1.0
+
+    klim = 5
+    wlim = 3
+    N = 100
+    dx = float(klim)/N
+    kvals = np.arange(0, klim+.01, dx)
+
+    if (b0_mag==0):
+        if (plot_or==1):
+            wvals = np.sqrt(1 + 3 * 0.1**2 * kvals**2)
+    #     else:
+    #         wvals = np.sqrt(1 + kvals**2)
+    # elif (plot_or==3):
+    #     wvals = np.sqrt(1 + kvals**2)
+    else:
+        wvals = np.sqrt(1 + kvals**2)
+
+    wR = np.array([0.5 * ( w_c + np.sqrt(w_c**2 + 4 * w_p**2))
+                   for i in np.arange(len(kvals))])        # right-handed cutoff
+    wL = np.array([0.5 * (-w_c + np.sqrt(w_c**2 + 4 * w_p**2))
+                   for i in np.arange(len(kvals))])        # left-handed cutoff
+    w_cvals = np.array([w_c for i in np.arange(len(kvals))])
+
+    # create figure
+    plt.figure()
+    plotme(hdf5_data)
+    plt.title(TITLE + ' w-k space' + ' e' + str(plot_or))
+    plt.xlabel('k  [$\omega_{pe}$/c]')
+    plt.ylabel('$\omega$  [$\omega_{pe}$]')
+    plt.xlim(0,klim)
+    plt.ylim(0,wlim)
+    if (show_theory==True):
+        # for i in range(1,10):
+        #     plt.plot(kvals, i*w_cvals, 'w--', label='')
+        plt.plot(kvals, wR, 'b--', label='$\omega$$_R$, right-handed cutoff')
+        plt.plot(kvals, wL, 'r--', label='$\omega$$_L$, left-handed cutoff')
+        plt.plot(kvals, wvals,'b', label='')
+        plt.legend(loc=0)
+    plt.show()
+
+def get_ratio(PATH1, PATH2):
+    #Function gets ratio of hdf52 and hdf51 in w-k space
+    hdf51 = read_hdf(PATH1)
+    hdf52 = read_hdf(PATH2)
+
+    hdf51 = FFT_hdf5(hdf51)   # FFT the data (x-t -> w-k)
+    hdf52 = FFT_hdf5(hdf52)   # FFT the data (x-t -> w-k)
+
+    s = 20  #sensitivity
+    for i in range(hdf51.shape[0]):
+       for j in range(hdf51.shape[1]):
+           hdf51.data[i,j] = abs(hdf51.data[i,j]/hdf52.data[i,j])
+           if (hdf51.data[i,j]>s):
+               hdf51.data[i,j] = 0.0
+
+    return hdf51
+
+def plot_mode_hist(hdf5):
+    # plot mode history for a couple of modes
+    plt.figure()
+    plt.semilogy(hdf5.data[:,4])
+    plt.semilogy(hdf5.data[:,8])
+    plt.show()
